6db4050d9e5971c27c906f962bcfad825ae4105e
melo0502 <72190444+melo0502@users.noreply.github.com>
2020-10-28 20:21:43 +0900

    未ログイン時では新規投稿出来ない

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:destroy, :create, :update]
+  before_action :authenticate_user!, only: [:destroy, :create, :update, :new]
   before_action :move_to_index, only: :edit
 
   def index
